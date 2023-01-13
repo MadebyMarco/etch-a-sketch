93aee6a3b980a44b162834b9b2135f67ad1a8854
MadebyMarco <marco3xmedina@gmail.com>
2023-01-13 12:46:05 -0800

    Add flex Add classes css to grid squares

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let gridContainer = document.querySelector('.grid-container');
 function createGrid(number) {
     number = prompt('Enter a number greater than 16 but less than 100', 16);
     for(i = 0; i < number; i++) {
-        gridContainer.appendChild(document.createElement('div'));
+    let gridSquare = gridContainer.appendChild(document.createElement('div'));
+    gridSquare.classList.add('gridSquare');
+    gridSquare.textContent = i + 1;
 };
 };
