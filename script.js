10fb482a516b3774ad48adae0b216509022b5741
MadebyMarco <marco3xmedina@gmail.com>
2023-01-14 07:41:29 -0800

    Add new grid Button

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const gridContainer = document.querySelector('.grid-container');
+let counter = 0;
 function createGrid(number) {
+
     number = prompt('Enter a number greater than 16 but less than 100', 16);
+
     for(i = 0; i < number; i++) {
     let gridSquare = gridContainer.appendChild(document.createElement('div'));
     gridSquare.classList.add('gridSquare');
     gridSquare.textContent = i + 1;
-    
-};
+    };
+
 const gridSquares = document.querySelectorAll('.gridSquare');
 
 gridSquares.forEach(square => square.addEventListener('mouseover', (e) => {
     e.target.classList.add('hover');
 }));
+counter++;
 };
 
+const newGridButton = document.querySelector('button');
 
-/** How do we recognize the mouse is hovering? Need an event listener on each square for hover
- * How do we change the color? Add class
- * How will it revery back to normal color? Remove class
- * How will it know to remove class? Event listener*hover effect 
- 
- * 
- * 
-*/
+newGridButton.addEventListener('click', () => {
+
+    if (counter > 0) {
+        const gridSquares = document.querySelectorAll('.gridSquare');
+        gridSquares.forEach(square => square.remove());
+    }
+    createGrid();
+   
+    
+});
